21d7d1dedd9a952684e2a8dfe1794ac773fb19d5
tdingler <46494773+tdingler@users.noreply.github.com>
2020-04-24 17:06:07 -0400

    Update main.cpp
    Added working updateScore function - calls game over with a timer and puts score on LCD screen. Game over if the game is done
    Added working rgbLED function - sets the color of the LED according to if in a game or not.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include "ultrasonic.h"
 #include "uLCD_4DGL.h"
 
-Thread threadLCD, threadLED, threadGame;
-Mutex mtx;
+Thread threadLED, threadGame; //thread to update game and LED
+Mutex mtx; //mutex lock
+Timer t;
 
 
 PwmOut RGBLed_r(p21);
@@ -13,80 +14,74 @@ PwmOut RGBLed_g(p25);
 
 uLCD_4DGL uLCD(p9,p10,p11);
 
-//Global thread variables
+//Global thread variables and flags
 volatile int points = 0;
 volatile int prevPoints = -1;
-volatile int game = 0;
+volatile int game = 1; //game flag 
+volatile int gamePrev = 0; //previous game flag. 
 
 void dist(int distance)
 {
-    //code to execute when distance has changed
 
-    printf("Game: %d", game); 
-    if(distance < 70 && game) {
+    //printf("Game: %d", game);
+    mtx.lock();
+    if(distance < 70 && game == 1) { //range to be within and add point
         prevPoints = points;
-        ++points;
-        // wait(100);
+        ++points; //incrememnt points 
+        //wait(100); //oof don't wait it'll break
     }
 
-
-
-    printf("Points: %d \r\n", points);
-
-    printf("Distance %d mm\r\n", distance);
+    mtx.unlock();
 
 }
 
 ultrasonic mu(p6, p7, .1, 1, &dist); //call dist when distance changes
 
-
-void rgbLED(void)
+/*
+void rgbLED(void) - thread function
+Inputs: Uses global Game flag to set the correct color to indicate if 
+the game is happening or not
+Outputs: LED color, green for game, purple for game over
+*/
+void rgbLED(void) 
 {
     while(1) {
-        printf("In RGB");
-        mtx.lock();
-        if(game) {
-            printf("In if");
-            RGBLed_r = 32;
+        //printf("In RGB\n");
+        mtx.lock(); //mtx lock for global variables
+        if(game == 1) {
+            //printf("In if");
+            RGBLed_r = 0; //set color to green for game start
             RGBLed_g = 255;
-            RGBLed_b = 134;
-        } 
-        else 
-        {
-            printf("Not in If");
-            RGBLed_r = 200;
+            RGBLed_b = 0;
+        } else {
+            //printf("Not in If\n");
+            RGBLed_r = 200; //set color to purple for game over
             RGBLed_g = 0;
             RGBLed_b = 255;
         }
-        game = !game;
+        //game = !game;
         mtx.unlock();
         Thread::wait(500);
     }
 }
 
-void endGame()
-{
-    while(1) {
-        mtx.lock();
-        if(game) {
-            printf("waiting to exit");
-            wait(1000);
-            game = 0;
-            printf("Wait ended");
-            uLCD.locate(2,2);
-            uLCD.printf("Game over. Score: %d", points);
-            break;
+/*
+void updateScore(void) - thread function
+Inputs: Global game flag, int prevPoints, int points
+Outputs: Calls distance sensor function if the game is active (1)
+  Adjusts the uLCD screen to display the number of points. 
+  Timer checks for if 30 seconds have passed to end the game (game = 0). 
+  When the game ends, prints out Game over and score. 
 
-        }
-        mtx.lock();
-    }
-}
-
+*/
 void updateScore(void)
 {
     while(1) {
         mtx.lock();
-        if(game) {
+        if(game == 1) {
+            t.start();
+            mu.checkDistance();
+            //wait(100); 
             if(prevPoints != points) {
                 uLCD.locate(0,0);
                 uLCD.text_width(5);
@@ -94,34 +89,40 @@ void updateScore(void)
                 uLCD.printf("%d", points);
 
             }
-
+            if(t.read() > 30) {
+                game = 0;
+                uLCD.locate(0,0);
+                uLCD.text_width(3); 
+                uLCD.text_height(3); 
+                uLCD.printf("Game\n over.\nScore: %d", points);
+            }
+            printf("Game: %d, Time: %f \n", game, t.read()); 
         }
         mtx.unlock();
         //Thread::wait(500);
-
     }
 }
 
-
-
-
+/*
+int main()
+Starts up the sensor for distance checking for goals. Begins
+the threads to update the score/game and to change the light color. 
+*/
 int main()
 {
     //uLCD.background_color(BLUE);
     //uLCD.printf("are you wired correctly?");
     mu.startUpdates();
-    //game = 0;
-    //threadLED.start();
-    threadLCD.start(updateScore);
-    threadLED.start(rgbLED);
-    //threadGame.start(endGame);
+    
+    threadGame.start(updateScore); //start updating the score
+    threadLED.start(rgbLED); //start checking LED lights
 
     while(1) {
-
-        mu.checkDistance();
-
+        
+        /*
+        Use to set game flag to one?
+        */ 
     }
 }
-//thread.start(rgbLED);
 
 
